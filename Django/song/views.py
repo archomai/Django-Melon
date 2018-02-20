8439424566832d38f53922cdafeb9bb483bd05d7
Seungri Cho <pseudo.circuit@gmail.com>
2018-02-20 11:25:20 +0900

    song search GET 방식으로 구현

--- Django/song/views.py
+++ Django/song/views.py
@@ -44,40 +44,42 @@ def song_search(request):
     # Song의 title에 keyword가 포함되는 경
     #   를 모두 포함(or -> Q object)하는 쿼리셋을 'songs'에 할당
 
-    if request.method == 'POST':
-        # POST 요청에 전달된 데이터(input요소의 값들)중, name이 'keyword'인 input의 값
-        keyword = request.POST['keyword'].strip()
-        # keyword에 빈 값이 올 경우 결과 QuerySet을 할당하지 않도록 수정
-        if keyword:
-            # Song목록 중 title이 keyword를 포함하는 퀘리셋
-            songs = Song.objects.filter(
-                Q(album__title__contains=keyword) |
-                Q(album__artists__name__contains=keyword) |
-                Q(title__contains=keyword)
-            ).distinct()
-            context['songs'] = songs
+    # if request.method == 'GET':
+    # POST 요청에 전달된 데이터(input요소의 값들)중, name이 'keyword'인 input의 값
 
-            # songs_from_artists
-            # songs_from_albums
-            # songs_from_title
-            # 위 세변수에 더 위의 조건 3개에 부합하는 쿼리셋을 각각전달
-            # 세 변수를 이용해 검색 결과를 3단으로 분리해서 출력
-            # -> 아티스트로 검색한 노래 결과, 앨범으로 검색한 노래 결과, 제목으로 검색한 노래 결과
+    keyword = request.GET.get('keyword')
 
-            songs_from_artists = Song.objects.filter(
-                album__artists__name__contains=keyword
-            )
-            context['songs_from_artists'] = songs_from_artists
+    # keyword에 빈 값이 올 경우 결과 QuerySet을 할당하지 않도록 수정
+    if keyword:
+        # Song목록 중 title이 keyword를 포함하는 퀘리셋
+        songs = Song.objects.filter(
+            Q(album__title__contains=keyword) |
+            Q(album__artists__name__contains=keyword) |
+            Q(title__contains=keyword)
+        ).distinct()
+        context['songs'] = songs
 
-            songs_from_album = Song.objects.filter(
-                album__title__contains=keyword
-            )
-            context['songs_from_albums'] = songs_from_album
+        # songs_from_artists
+        # songs_from_albums
+        # songs_from_title
+        # 위 세변수에 더 위의 조건 3개에 부합하는 쿼리셋을 각각전달
+        # 세 변수를 이용해 검색 결과를 3단으로 분리해서 출력
+        # -> 아티스트로 검색한 노래 결과, 앨범으로 검색한 노래 결과, 제목으로 검색한 노래 결과
 
-            songs_from_title = Song.objects.filter(
-                title__contains=keyword
-            )
-            context['songs_from_title'] = songs_from_title
+        songs_from_artists = Song.objects.filter(
+            album__artists__name__contains=keyword
+        )
+        context['songs_from_artists'] = songs_from_artists
+
+        songs_from_album = Song.objects.filter(
+            album__title__contains=keyword
+        )
+        context['songs_from_albums'] = songs_from_album
+
+        songs_from_title = Song.objects.filter(
+            title__contains=keyword
+        )
+        context['songs_from_title'] = songs_from_title
 
     # 만약 method가 POST였다면 context에 'songs'가 채워진 상태,
     # GET이면 빙 상태로 render실행
