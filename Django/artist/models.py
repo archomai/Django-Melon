053e9c99a4a36670b0b6b96c7e385017d5e807b2
Seungri Cho <pseudo.circuit@gmail.com>
2018-02-22 17:27:38 +0900

    login 구현

--- Django/artist/models.py
+++ Django/artist/models.py
@@ -1,5 +1,8 @@
+
+
 from datetime import datetime
 from io import BytesIO
+from pathlib import Path
 
 import requests
 from django.core.files import File
@@ -10,7 +13,6 @@ from crawler.artist import ArtistData
 
 class ArtistManager(models.Manager):
     def update_or_create_from_melon(self, artist_id):
-        # 크롤링 코드. 자신이 작성한 크롤러를 사용해서 같은 데이터를 가져오면 됩니다
         artist = ArtistData(artist_id)
         artist.get_detail()
         name = artist.name
@@ -34,13 +36,15 @@ class ArtistManager(models.Manager):
             # 기타 혈액형값으로 설정
             blood_type = Artist.BLOOD_TYPE_OTHER
 
-        # artist_id가 melon_id에 해당하는 Artist가 이미 있다면
-        #   해당 Artist의 내용을 update
-        # 없으면 Artist를 생성
+        # url_img_cover는 이미지의 URL
         response = requests.get(url_img_cover)
+        # requests에 GET요청을 보낸 결과의 Binary data
         binary_data = response.content
+        # 파일처럼 취급되는 메모리 객체 temp_file를 생성
         temp_file = BytesIO()
+        # temp_file에 이진데이터를 기록
         temp_file.write(binary_data)
+        # 파일객체의 포인터를 시작부분으로 되돌림
         temp_file.seek(0)
 
         artist, artist_created = self.update_or_create(
@@ -55,8 +59,11 @@ class ArtistManager(models.Manager):
                 'blood_type': blood_type,
             }
         )
-        from pathlib import Path
+        # img_profile필드에 저장할 파일명을 전체 URL경로에서 추출 (Path라이브러리)
         file_name = Path(url_img_cover).name
+        # artist.img_profile필드의 save를 따로 호출, 이름과 File객체를 전달
+        #   (Django)File객체의 생성에는 (Python)File객체를 사용,
+        #           이 때 (Python)File객체처럼 취급되는 BytesIO를 사용
         artist.img_profile.save(file_name, File(temp_file))
         return artist, artist_created
 
@@ -81,14 +88,45 @@ class Artist(models.Model):
         null=True,
         unique=True,
     )
-    img_profile = models.ImageField('프로필 이미지', upload_to='artist', blank=True)
-    name = models.CharField('이름', max_length=50)
-    real_name = models.CharField('본명', max_length=30, blank=True)
-    nationality = models.CharField('국적', max_length=50, blank=True)
-    birth_date = models.DateField('생년월일', blank=True, null=True)
-    constellation = models.CharField('별자리', max_length=30, blank=True)
-    blood_type = models.CharField('혈액형', max_length=1, choices=CHOICES_BLOOD_TYPE, blank=True)
-    intro = models.TextField('소개', blank=True)
+    img_profile = models.ImageField(
+        '프로필 이미지',
+        upload_to='artist',
+        blank=True,
+    )
+    name = models.CharField(
+        '이름',
+        max_length=50,
+    )
+    real_name = models.CharField(
+        '본명',
+        max_length=30,
+        blank=True,
+    )
+    nationality = models.CharField(
+        '국적',
+        max_length=50,
+        blank=True,
+    )
+    birth_date = models.DateField(
+        '생년월일',
+        blank=True,
+        null=True,
+    )
+    constellation = models.CharField(
+        '별자리',
+        max_length=30,
+        blank=True,
+    )
+    blood_type = models.CharField(
+        '혈액형',
+        max_length=1,
+        choices=CHOICES_BLOOD_TYPE,
+        blank=True,
+    )
+    intro = models.TextField(
+        '소개',
+        blank=True,
+    )
 
     objects = ArtistManager()
 
